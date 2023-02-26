24dbf2739902a42acb5fb3ad2633736f48773691
Hung Bui <hungbui7690@gmail.com>
2023-02-26 04:05:32 -0800

    Express: Environment Variables

--- app.js
+++ app.js
@@ -9,4 +9,7 @@ app.get('/api/course', (req, res) => {
   res.send([1, 2, 3])
 })
 
-app.listen(3000, () => console.log('Listening on port 3000...'))
+// setup port in env > export PORT=5000
+const port = process.env.PORT || 3000
+
+app.listen(3000, () => console.log(`Listening on port ${port}...`))
