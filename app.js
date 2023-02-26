84c6c2c852874904b08ab22e6932bd8853127f5d
Hung Bui <hungbui7690@gmail.com>
2023-02-26 04:17:16 -0800

    Express: Route Parameters
    Details:
    - req.params
    - req.query

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ app.get('/api/course', (req, res) => {
   res.send([1, 2, 3])
 })
 
+// req.params
+app.get('/api/course/:id', (req, res) => {
+  const params = req.params
+  res.send(params)
+})
+
+// multi params
+// req.query > /api/posts/2022/12/?name=joe&age=25
+app.get('/api/posts/:year/:month', (req, res) => {
+  const params = req.params
+  const query = req.query
+  res.send({ params, query })
+})
+
 // setup port in env > export PORT=5000
 const port = process.env.PORT || 3000
 
