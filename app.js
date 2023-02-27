b63148fcc9463d400ec6b5f51b4553afc6ef8f1a
Hung Bui <hungbui7690@gmail.com>
2023-02-26 21:21:43 -0800

    Async JS: Replace Callbacks with Promise

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
-// *** Named Function
+// *** Replace Callbacks with Promises
 
 console.log('Before')
 
-// 3.
-getUser(1, getAllRepos)
+// (4) we can see that, it does not work here > to consume promise, we need to use .then() & .catch
+getUser(1, (user) => {
+  getRepositories(user, (repo) => {
+    getCommits(repo, (commit) => {
+      console.log(commit)
+    })
+  })
+})
 console.log('After')
 
-// 2.
-function getAllRepos(user) {
-  getRepositories(user, getAllCommits)
+// (1)
+function getUser(id) {
+  // [a] return Promise + remove callback param
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Reading a User from database...')
+
+      // [b] change to resolve()
+      resolve({ id, githubUser: 'Joe' })
+    }, 2000)
+  })
 }
 
-// 1. don't use arrow function > hoisting
-function getAllCommits(repo) {
-  getCommits(repo, displayCommit)
+// (2) do similar to getUser
+function getRepositories(user) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Getting Repositories...')
+      resolve(['repo1', 'repo2', 'repo3'])
+    }, 2000)
+  })
 }
 
-function displayCommit(commit) {
-  console.log(commit)
-}
-
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('Reading a User from database...')
-    callback({ id, githubUser: 'Joe' })
-  }, 2000)
-}
-
-function getRepositories(user, callback) {
-  setTimeout(() => {
-    console.log('Getting Repositories...')
-    callback(['repo1', 'repo2', 'repo3'])
-  }, 2000)
-}
-
-function getCommits(repo, callback) {
-  setTimeout(() => {
-    console.log('Getting Commits...')
-    callback(['commit 1', 'commit 2', 'commit 3', 'commit 4'])
-  }, 2000)
+// (3) do similar to getUser
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Getting Commits...')
+      resolve(['commit 1', 'commit 2', 'commit 3', 'commit 4'])
+    }, 2000)
+  })
 }
--- promise.js
+++ promise.js
@@ -1,16 +1,12 @@
-// node promise.js
-
-// (1) create promise (async function)
 const p = new Promise((resolve, reject) => {
   let err = false
 
   // kick out some async work ...
   setTimeout(() => {
     if (!err) {
-      resolve({ user: 'Joe Doe' }) // pending => resolved/fulfilled
-    } else reject(new Error('Error Message')) // pending => rejected
+      resolve({ user: 'Joe Doe' })
+    } else reject(new Error('Error Message'))
   }, 2000)
 })
 
-// (2) consume promise
 p.then((result) => console.log(result)).catch((err) => console.log(err))
