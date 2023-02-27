560696ef2ec83d1e845ec818fa6015498da2e831
Hung Bui <hungbui7690@gmail.com>
2023-02-27 01:47:20 -0800

    Mongoose: Connect to DB

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+const mongoose = require('mongoose')
 
-app.get('/api/course', (req, res) => {
-  res.send([1, 2, 3])
-})
+// We don't need to create database in MongoDb > we just need to work here, then after create collection, Mongoose will create the DB for us
+mongoose.connect('mongodb://localhost/playground')
 
 app.listen(3000, () => console.log('Listening on port 3000...'))
