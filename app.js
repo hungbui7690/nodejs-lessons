7263f416012e68139f897ccbc8ea440670bd0bac
Hung Bui <hungbui7690@gmail.com>
2023-02-27 01:52:25 -0800

    Mongoose: Catch Error + Log out when Connect to DB

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express()
 const mongoose = require('mongoose')
 
 // We don't need to create database in MongoDb > we just need to work here, then after create collection, Mongoose will create the DB for us
-mongoose.connect('mongodb://localhost/playground')
+mongoose
+  .connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch((err) => console.log(`Could not connect to MongoDB...`, err))
 
 app.listen(3000, () => console.log('Listening on port 3000...'))
