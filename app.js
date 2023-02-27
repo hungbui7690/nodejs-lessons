a0d9a15030871a0aee9b89865968ad050e390479
Hung Bui <hungbui7690@gmail.com>
2023-02-27 01:59:25 -0800

    Mongoose: Schema

--- app.js
+++ app.js
@@ -2,11 +2,34 @@ const express = require('express')
 const app = express()
 
 const mongoose = require('mongoose')
+const { boolean } = require('webidl-conversions')
 
-// We don't need to create database in MongoDb > we just need to work here, then after create collection, Mongoose will create the DB for us
+////////////////////////////////////////////////
+// CONNECT TO MONGODB
+////////////////////////////////////////////////
+
 mongoose
   .connect('mongodb://localhost/playground')
   .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.log(`Could not connect to MongoDB...`, err))
+
+////////////////////////////////////////////////
+// SCHEMA
+////////////////////////////////////////////////
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String], // array of string
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  isPublished: boolean,
+})
+
+////////////////////////////////////////////////
+// SERVER
+////////////////////////////////////////////////
 
 app.listen(3000, () => console.log('Listening on port 3000...'))
