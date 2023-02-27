d29c16fb8ea81a23cf016c5c63de45321478b148
Hung Bui <hungbui7690@gmail.com>
2023-02-26 20:26:58 -0800

    Async JS: Async Pattern

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+// with async function, we can not consume it like normal
+// there are 3 ways to consume an async function:
+// - callback
+// - promise
+// - async/await
+
 console.log('Before')
-setTimeout(() => {
-  console.log('Reading a User from database...')
-})
+const user = getUser(1)
+console.log(user)
 console.log('After')
+
+function getUser(id) {
+  setTimeout(() => {
+    console.log('Reading a User from database...')
+    return { id, githubUser: 'Joe' }
+  })
+}
