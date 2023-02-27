57284805ec2dd6c38abc8e6b68d2f6584827f486
Hung Bui <hungbui7690@gmail.com>
2023-02-26 20:56:45 -0800

    Async JS: Named Function P1

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-// Using Callback
+// To solve Callback Hell when consuming Async Functions, there are 3 ways:
+// a. Named Function
+// b. Promise
+// c. Async/Await
 
 console.log('Before')
-
-// Callback Hell
 getUser(1, (user) => {
   getRepositories(user, (repo) => {
-    getCommits(repo, (commit) => {
-      console.log(commit)
-    })
+    // 2. pass the reference here
+    getCommits(repo, displayCommit)
   })
 })
 console.log('After')
+
+// 1.
+function displayCommit(commit) {
+  console.log(commit)
+}
 
 function getUser(id, callback) {
   setTimeout(() => {
