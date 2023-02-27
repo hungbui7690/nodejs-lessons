f26e7abd445df834926a80b0c376f2b6a2f4cc4f
Hung Bui <hungbui7690@gmail.com>
2023-02-27 06:03:41 -0700

    Mongoose: Exercise

---
+++ exercise.js
@@ -0,0 +1,6 @@
+//////////////////////////////////////////
+// run this to import data to mongodb (need to install mongodb import-tool)
+// > mongoimport --db mongo-exercises --collection courses --drop --file exercise-data.json --jsonArray
+
+// Exercise:
+// Get all the published backend courses, sort them by their name, pick only their name and author, and display them
