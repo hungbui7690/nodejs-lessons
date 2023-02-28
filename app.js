6c15f79f1d0d88fc5445dc10fc81d5c179cd64e3
Hung Bui <hungbui7690@gmail.com>
2023-02-28 02:01:37 -0700

    Stream: Pipe P1
    Details:
    - pipe()

--- app.js
+++ app.js
@@ -1,36 +1,10 @@
-// High Water Mark:
-// - check pic: overflow
-// - back pressure: whenever we have the hose (pipe)
-// - high water mark: how much water the hose can handle
-
-const { createReadStream, createWriteStream } = require('fs')
+// PIPE, DUPLEX & TRANSFORM STREAMS
+const { createReadStream, createWriteStream, write } = require('fs')
 const readStream = createReadStream('./Funny Cat.mp4')
+const writeStream = createWriteStream('./copy.mp4')
 
-// (***)
-const writeStream = createWriteStream('./copy.mp4', { highWaterMark: 1234567 }) // more memory intensive
-
-readStream.on('data', (chunk) => {
-  const result = writeStream.write(chunk)
-
-  if (!result) {
-    console.log('back pressure')
-    readStream.pause()
-  }
-})
-
-readStream.on('error', (err) => {
-  console.log('An Error has Occurred', err)
-})
-
-readStream.on('end', () => {
-  writeStream.end()
-})
-
-writeStream.on('drain', () => {
-  console.log('drained')
-  readStream.resume()
-})
-
-writeStream.on('close', () => {
-  process.stdout.write('file copied \n')
-})
+// pipe() will:
+// - create all the pipe between those 2 streams
+// - also handle back pressure for us
+// - we just need to handle error
+readStream.pipe(writeStream).on('error', console.error)
--- writableStream.js
+++ writableStream.js
@@ -3,12 +3,24 @@
 // - back pressure: whenever we have the hose (pipe)
 // - high water mark: how much water the hose can handle
 
+// High Water Mark:
+// - check pic: overflow
+// - back pressure: whenever we have the hose (pipe)
+// - high water mark: how much water the hose can handle
+
 const { createReadStream, createWriteStream } = require('fs')
 const readStream = createReadStream('./Funny Cat.mp4')
-const writeStream = createWriteStream('./copy.mp4')
+
+// (***)
+const writeStream = createWriteStream('./copy.mp4', { highWaterMark: 1234567 }) // more memory intensive
 
 readStream.on('data', (chunk) => {
-  writeStream.write(chunk)
+  const result = writeStream.write(chunk)
+
+  if (!result) {
+    console.log('back pressure')
+    readStream.pause()
+  }
 })
 
 readStream.on('error', (err) => {
@@ -17,6 +29,11 @@ readStream.on('error', (err) => {
 
 readStream.on('end', () => {
   writeStream.end()
+})
+
+writeStream.on('drain', () => {
+  console.log('drained')
+  readStream.resume()
 })
 
 writeStream.on('close', () => {
