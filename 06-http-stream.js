43c848c429a2f79eff48dada157a1c81e2d5637a
Hung Bui <hungbui7690@gmail.com>
2023-02-28 10:29:17 -0700

    Stream: HTTP Stream P5: create routes with Express
    Details:
    - the problem is it output always show: ------WebKitFormBoundaryOsqd4Qva6NQFMI5B

--- 06-http-stream.js
+++ 06-http-stream.js
@@ -1,56 +1,47 @@
 // HTTP STREAM
 
-const express = require('express')
-const app = express()
-const { stat, createReadStream, createWriteStream } = require('fs')
+const http = require('http')
+const { stat, createReadStream } = require('fs')
 const { promisify } = require('util')
 const file = './Funny Cat.mp4'
 const fileInfo = promisify(stat)
 
-app.use(express.urlencoded({ extended: false }))
-
-// (1)
-const sendVideo = async (req, res) => {
+/*
+  The problem now is we cannot skip to the end of the video 
+*/
+const server = http.createServer(async (req, res) => {
   const { size } = await fileInfo(file)
-  const range = req.headers.range
 
+  // (1) bytes=0- > from 0 to ...
+  const range = req.headers.range
+  console.log(range)
+
+  // (2) handle range
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Range_requests
   if (range) {
+    // (a)
     let [start, end] = range.replace(/bytes=/, '').split('-')
     start = parseInt(start, 10)
     end = end ? parseInt(end, 10) : size - 1
 
+    // (b)
     res.writeHead(206, {
       'Content-Range': `bytes ${start}-${end}/${size}`,
       'Accept-Ranges': 'bytes',
       'Content-Length': end - start + 1,
       'Content-Type': 'video/mp4',
     })
+
+    // (c)
     createReadStream(file, { start, end }).pipe(res)
   } else {
     res.writeHead(200, {
       'Content-Type': 'video/mp4',
       'Content-Length': size,
     })
+
     createReadStream(file).pipe(res)
   }
-}
-
-// (2)
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '\\index.html')
 })
 
-// (3)
-app.get('/video', (req, res) => {
-  sendVideo(req, res)
-})
-
-// (4) handle form submission
-// (***) problem: always show ------WebKitFormBoundaryOsqd4Qva6NQFMI5B--
-app.post('/', (req, res) => {
-  req.pipe(res) // display in browser
-  req.pipe(process.stdout) // display in console
-  req.pipe(createWriteStream('./uploaded.txt')) // save file
-})
-
-app.listen(3000, () => console.log('Server is Running on Port 3000...'))
+server.listen(3000, () => console.log('Server is Running on Port 3000...'))
