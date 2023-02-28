a8cd9cde150a372d88382f53edaaabb4700979f8
Hung Bui <hungbui7690@gmail.com>
2023-02-27 23:24:12 -0700

    Stream: Write Stream P3: back pressure

--- writableStream.js
+++ writableStream.js
@@ -1,4 +1,4 @@
-// Back Pressure:
+// WRITABLE STREAM:
 // - check pic: overflow
 // - back pressure: whenever we have the hose (pipe)
 // - high water mark: how much water the hose can handle
@@ -8,13 +8,7 @@ const readStream = createReadStream('./Funny Cat.mp4')
 const writeStream = createWriteStream('./copy.mp4')
 
 readStream.on('data', (chunk) => {
-  // (1) return true/false > hose is full or not
-  const result = writeStream.write(chunk)
-
-  if (!result) {
-    console.log('back pressure')
-    readStream.pause()
-  }
+  writeStream.write(chunk)
 })
 
 readStream.on('error', (err) => {
@@ -23,12 +17,6 @@ readStream.on('error', (err) => {
 
 readStream.on('end', () => {
   writeStream.end()
-})
-
-// (2)
-writeStream.on('drain', () => {
-  console.log('drained')
-  readStream.resume()
 })
 
 writeStream.on('close', () => {
