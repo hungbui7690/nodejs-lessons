6345dda83ecbf61213bdfaf09fe466b14dcddbeb
Hung Bui <hungbui7690@gmail.com>
2023-02-27 23:26:33 -0700

    Stream: Writable Stream: high water mark

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-// Back Pressure:
+// High Water Mark:
 // - check pic: overflow
 // - back pressure: whenever we have the hose (pipe)
 // - high water mark: how much water the hose can handle
 
 const { createReadStream, createWriteStream } = require('fs')
 const readStream = createReadStream('./Funny Cat.mp4')
-const writeStream = createWriteStream('./copy.mp4')
+
+// (***)
+const writeStream = createWriteStream('./copy.mp4', { highWaterMark: 1234567 }) // more memory intensive
 
 readStream.on('data', (chunk) => {
-  // (1) return true/false > hose is full or not
   const result = writeStream.write(chunk)
 
   if (!result) {
@@ -25,7 +26,6 @@ readStream.on('end', () => {
   writeStream.end()
 })
 
-// (2)
 writeStream.on('drain', () => {
   console.log('drained')
   readStream.resume()
