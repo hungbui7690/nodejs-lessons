31c6a549c8206e052e400798f4ee4f4b96ad0325
Hung Bui <hungbui7690@gmail.com>
2023-02-27 19:54:11 -0700

    Stream: Readable Stream P5: non-flowing mode

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const readStream = fs.createReadStream('./Funny Cat.mp4')
 
+// Convert to non-flowing stream
+// (1)
 readStream.on('data', (chunk) => {
   console.log(`> Little Chunk ${chunk.length} : `, chunk)
 })
@@ -11,4 +13,13 @@ readStream.on('end', () => {
 
 readStream.on('error', (err) => {
   console.log('An Error has Occurred', err)
+})
+
+// (2)
+readStream.pause()
+
+// (3)
+process.stdin.on('data', (chunk) => {
+  if (chunk.toString().trim() === 'q') readStream.resume()
+  readStream.read()
 })
