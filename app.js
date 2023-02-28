34d686647e82e838a50028278ed72a476f9068e0
Hung Bui <hungbui7690@gmail.com>
2023-02-27 19:49:18 -0700

    Stream: Readable Stream P4: how to use

--- app.js
+++ app.js
@@ -1,41 +1,14 @@
-const { Readable } = require('stream')
+const fs = require('fs')
+const readStream = fs.createReadStream('./Funny Cat.mp4')
 
-// READABLE STREAM
+readStream.on('data', (chunk) => {
+  console.log(`> Little Chunk ${chunk.length} : `, chunk)
+})
 
-const funnies = [
-  'My mother always used to say: The older you get, the better you get, unless you’re a banana.',
-  'Clothes make the man. Naked people have little or no influence in society',
-  "I love being married. It's so great to find that one special person you want to annoy for the rest of your life",
-  'Never follow anyone else’s path. Unless you’re in the woods and you’re lost and you see a path. Then by all means follow that path.',
-  'I walk around like everything’s fine, but deep down, inside my shoe, my sock is sliding off.',
-]
+readStream.on('end', () => {
+  console.log('[][] Read Stream Ended')
+})
 
-// (***) This is bts
-class StreamFromArray extends Readable {
-  constructor(array) {
-    super({ objectMode: true }) // object mode
-    this.array = array
-    this.index = 0
-  }
-
-  // object mode
-  _read() {
-    if (this.index <= this.array.length) {
-      const chunk = {
-        data: this.array[this.index],
-        index: this.index,
-      }
-      // push chunk into pipeline
-      this.push(chunk)
-      this.index++
-    } else {
-      this.push(null)
-    }
-  }
-}
-
-const stream = new StreamFromArray(funnies)
-
-stream.on('data', (chunk) => console.log(chunk))
-
-stream.on('end', () => console.log(`End of Stream`))
+readStream.on('error', (err) => {
+  console.log('An Error has Occurred', err)
+})
