e9229d4fafa8f6d6d632c584a9a4bb2d1cccbbad
Hung Bui <hungbui7690@gmail.com>
2023-02-28 02:45:36 -0700

    Stream: HTTP Stream P2: send video to browser

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // HTTP STREAM
 
-const { createServer } = require('http')
+const http = require('http')
 const { createReadStream } = require('fs')
 const file = './Funny Cat.mp4'
 
-createServer((req, res) => {}).listen(3000, () =>
-  console.log('Server is Running on Port 3000...')
-)
+const server = http.createServer((req, res) => {
+  // (1)
+  res.writeHead(200, {
+    'Content-Type': 'video/mp4',
+  })
+
+  // (2)
+  createReadStream(file).pipe(res)
+})
+
+server.listen(3000, () => console.log('Server is Running on Port 3000...'))
